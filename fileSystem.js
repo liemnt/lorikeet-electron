1eba0bff1c242084b36081a3bf6bfdcd20e7be1a
Nguyễn Thanh Liêm <liemnt.uit@gmail.com>
2020-09-09 11:19:42 +0700

    search feature

--- fileSystem.js
+++ fileSystem.js
@@ -1,21 +1,32 @@
 'use strict';
+
 const async = require('async');
 const fs = require('fs');
 const osenv = require('osenv');
 const path = require('path');
 
+let shell;
+
+if (process.versions.electron) {
+    shell = require('electron').shell;
+} else {
+    shell = window.require('nw.gui').Shell;
+}
+
 function getUsersHomeFolder() {
     return osenv.home();
 }
+
 function getFilesInFolder(folderPath, cb) {
     fs.readdir(folderPath, cb);
 }
+
 function inspectAndDescribeFile(filePath, cb) {
     let result = { file: path.basename(filePath), path: filePath, type: '' };
     fs.stat(filePath, (err, stat) => {
         if (err) {
             cb(err);
-        } else {
+        }       else {
             if (stat.isFile()) {
                 result.type = 'file';
             }
@@ -26,14 +37,21 @@ function inspectAndDescribeFile(filePath, cb) {
         }
     });
 }
+
 function inspectAndDescribeFiles(folderPath, files, cb) {
     async.map(files, (file, asyncCb) => {
         let resolvedFilePath = path.resolve(folderPath, file);
         inspectAndDescribeFile(resolvedFilePath, asyncCb);
     }, cb);
 }
+
+function openFile(filePath) {
+    shell.openItem(filePath);
+}
+
 module.exports = {
     getUsersHomeFolder,
     getFilesInFolder,
-    inspectAndDescribeFiles
+    inspectAndDescribeFiles,
+    openFile
 };
